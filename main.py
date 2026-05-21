dc5e9f54befded68ba4a5b96482f5f22c0a37696
MasciaJ <jmasciamarquez@alumno.huergo.edu.ar>
2026-05-21 13:35:26 -0300

    insertion sort

--- main.py
+++ main.py
@@ -28,10 +28,26 @@ def selection_sort(lista):
         arr[i], arr[min_index] = arr[min_index], arr[i]
     return arr
 
+def insertion_sort(lista):
+    arr = lista.copy()
+    n = len(arr)
+
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+    return arr
+
 tamaños = [100, 200, 400, 800, 1000]
 
 tiempos_bubble = []
 tiempos_selection = []
+tiempos_insertion = []
 
 for tamaño in tamaños:
 
@@ -47,9 +63,15 @@ for tamaño in tamaños:
     fin = time.time()
     tiempos_selection.append(fin - inicio)
 
+    inicio = time.time()
+    insertion_sort(lista)
+    fin = time.time()
+    tiempos_insertion.append(fin - inicio)
+
 plt.plot(tamaños, tiempos_bubble, marker="o", label="Bubble Sort")
 plt.plot(tamaños, tiempos_selection, marker="o", label="Selection Sort")
-plt.title("Bubble Sort vs Selection Sort")
+plt.plot(tamaños, tiempos_insertion, marker="o", label="Insertion Sort")
+plt.title("Sorting Algorithms Comparison")
 plt.xlabel("Cantidad de elementos")
 plt.ylabel("Tiempo de ejecución (segundos)")
 plt.legend()
